aefeca1d4dd554f6bfea9dfacc19317cee91e1e2
BeLeap <beleap@beleap.dev>
2023-05-16 10:38:14 +0900

    init discord bot

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use dotenvy::dotenv;
+use serenity::{prelude::GatewayIntents, Client};
 use std::env;
 
 #[tokio::main]
 async fn main() {
     dotenv().expect(".env file not found");
-
     let discord_token = match env::var("DISCORD_TOKEN") {
         Ok(value) => value,
         Err(e) => panic!("{}", e),
     };
-    println!("{}", discord_token);
+
+    let mut client = Client::builder(&discord_token, GatewayIntents::empty())
+        .await
+        .expect("Error creating client");
+
+    if let Err(e) = client.start().await {
+        eprintln!("Client error: {:?}", e);
+    }
 }
