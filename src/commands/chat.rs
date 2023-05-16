980e73a3a4be8e53bf4446e2e305868635602638
BeLeap <beleap@beleap.dev>
2023-05-16 13:42:19 +0900

    add option for chat command

--- src/commands/chat.rs
+++ src/commands/chat.rs
@@ -1,9 +1,22 @@
-use serenity::builder::CreateApplicationCommand;
+use serenity::{
+    builder::CreateApplicationCommand,
+    model::prelude::command::{CommandOptionType, CommandType},
+};
 
 pub fn run() -> String {
     "chit".to_string()
 }
 
 pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
-    command.name("chat")
+    command
+        .name("chat")
+        .description("Chat with bard")
+        .kind(CommandType::ChatInput)
+        .create_option(|option| {
+            option
+                .name("instruction")
+                .description("Instruction for bard")
+                .kind(CommandOptionType::String)
+                .required(true)
+        })
 }
