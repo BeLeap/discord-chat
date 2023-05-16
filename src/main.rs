b5a685bcde58cb0c0a7b0efc7ac8a3cf8c5b2151
BeLeap <beleap@beleap.dev>
2023-05-16 09:55:32 +0900

    use dotenvy for token

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use dotenvy::dotenv;
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    dotenv().expect(".env file not found");
+
+    let discord_token = match env::var("DISCORD_TOKEN") {
+        Ok(value) => value,
+        Err(e) => panic!("{}", e),
+    };
+    println!("{}", discord_token);
 }
