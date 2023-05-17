1e1f5784d380039f1d25f3d1fd95dddc0ba09eb7
BeLeap <beleap@beleap.dev>
2023-05-17 16:50:17 +0900

    updating prompt

--- src/api/cohere.rs
+++ src/api/cohere.rs
@@ -59,6 +59,13 @@ impl fmt::Display for CohereErrorKind {
 #[derive(Debug, Serialize)]
 struct CohereRequest {
     prompt: String,
+    max_tokens: u32,
+    return_likelihoods: ReturnLikelihoods,
+}
+
+#[derive(Debug, Serialize)]
+enum ReturnLikelihoods {
+    NONE,
 }
 
 #[derive(Debug, Deserialize)]
@@ -77,15 +84,21 @@ impl Chattable for Cohere {
 
     async fn chat(&self, instruction: String) -> Result<String, CohereError> {
         let prompt = format!(
-            "Below is an instruction that describes a task. Write a response that appropriately completes the request.
-### Instruction:
-{}
+            "Transcript of a dialog, where the User interacts with an Assistant named Bob. Bob is helpful, kind, honest, good at writing, and never fails to answer the User's requests immediately and with precision.
 
-### Response:
-        ",
+User: Hello, Bob.
+Bob: Hello. How may I help you today?
+User: Please tell me the largest city in Europe.
+Bob: Sure. The largest city in Europe is Moscow, the capital of Russia.
+User: {}
+Bob:",
             instruction
         );
-        let request = CohereRequest { prompt };
+        let request = CohereRequest {
+            prompt,
+            max_tokens: 300,
+            return_likelihoods: ReturnLikelihoods::NONE,
+        };
 
         let request = self
             .client
